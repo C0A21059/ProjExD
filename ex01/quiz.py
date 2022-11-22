2506c398f4c305870c7dfbc83b7f0683bb62e97f
Sadano <c0a21059af@edu.teu.ac.jp>
2022-11-22 16:08:30 +0900

    new

---
+++ ex01/quiz.py
@@ -0,0 +1,27 @@
+import random
+import datetime
+
+def shutdai(qa_lst):
+    quetion,ans = random.choice(list(qa_lst.items()))
+    print(f"問題:\n{quetion}")
+    return ans
+
+def kaitou(ans):
+    st = datetime.datetime.now()
+    ans_input = input("答えよ：")
+    ed = datetime.datetime.now()
+    if ans_input in ans:
+        return f"{(ed-st).seconds}秒かかっているが正解だ"
+    else:
+        return "黙れ"
+
+def main(quetsiton_ans):
+    ans = shutdai(quetsiton_ans)
+    print(kaitou(ans))
+
+
+if __name__ == "__main__":
+    qa_lst = {"サザエの旦那の名前は？":("マスオ", "ますお"),
+                "カツオの妹の名前は？":("ワカメ", "わかめ"),
+                "タラオはカツオから見てどんな関係？":("甥", "おい", "甥っ子", "おいっこ")}
+    main(qa_lst)
