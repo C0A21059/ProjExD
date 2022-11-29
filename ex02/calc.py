92bb32e1fa819a5cf104be8f57475c90a5becfed
Sadano <c0a21059af@edu.teu.ac.jp>
2022-11-29 15:32:40 +0900

    追加　四則演算

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,30 +7,34 @@ def button_click(event):
     if num =="=":
         siki = entry.get()
         ans = eval(siki)
-        entry.delete(0,tk.END)
-        entry.insert(tk.END,ans)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, ans)
     else:
-        entry.insert(tk.END,num)
+        entry.insert(tk.END, num)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("tk")
-    root.geometry("300x500")
+    root.geometry("400x600")
     entry = tk.Entry(justify="right",width=10,font=("",40))
-    entry.grid(row=0,column=0,columnspan=3)
+    entry.grid(row=0,column=0,columnspan=4)
 
     num = 0
     for i in range(10):
-
-        button = tk.Button(root,text=f"{9-num}",font=("",30),width=4,height=2,bg="#ffffff")
-        button.grid(row = num//3+1, column=num%3)
-        button.bind("<1>",button_click)
+        if i ==9:
+            button = tk.Button(root,text=f"{9-num}",font=("",30), width = 4, height = 2, bg="#ffffff")#数字の色変更
+            button.grid(row = num // 3 + 1, column = num % 3 +1)
+            button.bind("<1>", button_click)
+        else:
+            button = tk.Button(root,text=f"{9-num}",font=("",30), width = 4, height = 2, bg="#ffffff")#数字の色変更
+            button.grid(row = num // 3 + 1, column = num % 3)
+            button.bind("<1>", button_click)
         num += 1
 
-    option = ["+","="]#演算子の追加はここに
+    option = ["/","*","-","+","="]#演算子の追加はここに
     for op in option:
-        button = tk.Button(root,text=f"{op}",font=("",30),width=4,height=2)
-        button.grid(row = num//3+1, column=num%3)
-        button.bind("<1>",button_click)
-        num +=1
+        button = tk.Button(root,text=f"{op}", font=("", 30), width = 4, height = 2)
+        button.grid(row = num % len(option) + 1, column = 4)
+        button.bind("<1>", button_click)
+        num += 1
     root.mainloop()
