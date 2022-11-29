09b14ddcc82dec9714d4b2e270590810b62471af
Sadano <c0a21059af@edu.teu.ac.jp>
2022-11-29 14:32:14 +0900

    練習2

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,5 +5,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("tk")
     root.geometry("300x500")
+    for i in range(10):
+
+        button = tk.Button(root,text=f"{9-i}",font=("",30),width=4,height=2)
+        button.grid(row = i//3, column=i%3)
 
     root.mainloop()
