cba69b575013317ec77a3a627d077ee00ed40876
Sadano <c0a21059af@edu.teu.ac.jp>
2022-12-06 13:41:28 +0900

    キー入力

--- ex03/sample.py
+++ ex03/sample.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 def count_up():
     global tmr
@@ -6,11 +7,17 @@ def count_up():
     tmr = tmr+1
     root.after(1000,count_up)
 
+def key_down(event):
+    key = event.keysym
+    root.after(1000,count_up)
+    tkm.showinfo("キー押下", f"{key}キーが押されました")
+
 if __name__ == "__main__":
     root = tk.Tk()
     label= tk.Label(root, text="-", font=("", 80))
     label.pack()
 
     tmr = 0
-    count_up()
+    #count_up()
+    root.bind("<KeyPress>", key_down)
     root.mainloop()
