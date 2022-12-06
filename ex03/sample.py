02af6c4c8241c0b4a6db47fe85ad839814dac5c5
Sadano <c0a21059af@edu.teu.ac.jp>
2022-12-06 13:57:27 +0900

    リアルタイム処理中断

--- ex03/sample.py
+++ ex03/sample.py
@@ -2,15 +2,22 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 def count_up():
-    global tmr
+    global tmr,jid
     label["text"] = tmr
     tmr = tmr+1
-    root.after(1000,count_up)
+    jid = root.after(1000,count_up)
+    #root.after(1000,count_up)
 
 def key_down(event):
+    global jid
+    if jid is not None:
+        print(jid)
+        root.after_cancel(jid)
+        jid = None
     key = event.keysym
-    root.after(1000,count_up)
-    tkm.showinfo("キー押下", f"{key}キーが押されました")
+    jid = root.after(1000,count_up)
+    #root.after(1000,count_up)
+    tkm.showinfo(f"{key}", f"{key}")
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -18,6 +25,7 @@ if __name__ == "__main__":
     label.pack()
 
     tmr = 0
+    jid = None
     #count_up()
     root.bind("<KeyPress>", key_down)
     root.mainloop()
