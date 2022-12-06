4a49074ebadab82661e3395634f24ab702fb387a
Sadano <c0a21059af@edu.teu.ac.jp>
2022-12-06 14:29:00 +0900

    練習4

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,12 +3,15 @@ import tkinter as tk
 if __name__ == "__main__":
     cx = 300
     cy = 400
+    key = ""
 
     root = tk.Tk()
     root.title("迷えるこうかとん")
     image = tk.PhotoImage(file="fig/0.png")
 
     canvas = tk.Canvas(width=1500,height=900,bg="#000000")
-    canvas.create_image(cx, cy, image=image)
+    canvas.create_image(cx, cy,
+                        image=image,
+                        tag="kokaton")
     canvas.pack()
     root.mainloop()
